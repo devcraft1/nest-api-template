edf8ac6c8cf8fb2742446569521723cfcab57429
devcraft1 <ukonudennis64@gmail.com>
2023-05-05 23:55:10 -1100

    mail-service

--- src/mailgun/mailgun.service.ts
+++ src/mailgun/mailgun.service.ts
@@ -26,7 +26,8 @@ export type EmailOptions = MailgunMessageData;
 export class MailgunService {
   private readonly mailgun: Client;
   constructor(
-    @Inject(MAILGUN_CONFIGURATION) private readonly configuration: Options,
+    @Inject(MAILGUN_CONFIGURATION)
+    private readonly configuration: Options,
   ) {
     this.mailgun = new Mailgun(FormData).client(configuration);
   }
